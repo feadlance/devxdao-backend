48acf6f70808d3a3ba7b16d305fe9f0c53098c53
Davut Kember <davutkmbr@gmail.com>
2021-11-23 15:27:48 +0300

    Add comments relation

--- app/Survey.php
+++ app/Survey.php
@@ -7,16 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-	protected $table = 'survey';
+    protected $table = 'survey';
 
     protected $guarded = [];
 
-	public function surveyRanks() {
+    public function surveyRanks()
+    {
         return $this->hasMany('App\SurveyRank', 'survey_id', 'id');
     }
 
-    public function surveyDownvoteRanks() {
+    public function surveyDownvoteRanks()
+    {
         return $this->hasMany('App\SurveyDownVoteRank', 'survey_id', 'id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function getEndTimeAttribute($value)
